1f6e21c9370e591f3b334e57cbb178c575239399
Catry <catry.me@gmail.com>
2020-10-05 12:08:56 +0900

    add settings file

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,51 @@
 extern crate requests;
 extern crate webbrowser;
 
+use configparser::ini::Ini;
 use requests::ToJson;
 use download_rs::sync_download::Download;
-use std::io::{stdin,stdout,Write};
+use std::thread;
+use std::time::Duration;
 
-fn main()  {
-    let tag = "neko";
+fn main() {
+    let mut config = Ini::new();
+    let _map = config.load("settings.ini");
+    let _tags = vec!["neko", "neko-r19", "hentai", "hentai-gif"];
+    //print!("{}", _tags[0]);
+    let tag = config.get("DEFAULT", "tag").clone().unwrap();
     let mut main = String::from("https://img.trinets.xyz/static/");
     main.push_str(&(tag.to_owned()+ "/"));
     let mut img =  String::from("https://img.trinets.xyz/api/v1?target=");
     img.push_str(&(tag.to_owned() + "&type=list"));
     let response =  requests::get(img).unwrap();
-    let data = response.json().unwrap();
-    // (args[0], view, download) (args[1], img_number)
-
-    let mut a = String::new();
-    print!("view or download: ");
-    let _= stdout().flush();
-    stdin().read_line(&mut a).expect("Error");
-
-    //number or imageid (ex. 114fc734bb)
-    let mut s = String::new();
-    print!("Please enter some number or string: ");
-    let _= stdout().flush();
-    stdin().read_line(&mut s).expect("Error");
-
-    let num= s.trim();
-    let number:usize = match num.trim().parse() {
-        Ok(a) => a,
-        _ => unreachable!(),
-    };
-    let url = main.to_owned() + &data[number].clone().to_string();
-    let folder = "./tests/";
-    let filename = folder.to_owned() + &data[number].clone().to_string();
-
-    if a.to_lowercase().trim() == "view" {
-    // view
-    let _view = webbrowser::open(&url.to_string());
-
-    } else if a.to_lowercase().trim() == "download" {
-    // download
-    let download = Download::new(&url,Some(&filename),None);
-
-    match download.download() {
-        Ok(_) => println!("downloaded"),
-        Err(e) => println!("Error ： {}",e.to_string()),
+    if  response.is_json() == false {
+        return;
     }
-}
+    let data = response.json().unwrap();
+    let len:usize = data.len() - 1;
+    // (args[0], view, download) (args[1], img_number)
+    //number or imageid (ex. 114fc734bb)
+    /* let num= s.trim();
+    let number:usize =  num.trim().parse().unwrap_or(0); */
+    let num = config.get("DEFAULT", "number").clone().unwrap();
+    let number:usize = num.trim().parse().unwrap();
+    let url = main.to_owned() + &data[number].clone().to_string();
+    let folder = config.get("DEFAULT", "folder").clone().unwrap();
+    let filename = folder.to_owned() + &data[number].clone().to_string();
+    let choice = config.get("DEFAULT", "choice").clone().unwrap().trim().to_lowercase();
+
+    if choice  == "view" {
+        // view
+        let _view = webbrowser::open(&url.to_string());
+
+    } else if choice == "download" {
+        // download
+        let download = Download::new(&url,Some(&filename),None);
+
+        match download.download() {
+            Ok(_) => println!("downloaded"),
+            Err(e) => println!("Error ： {}",e.to_string()),
+        }
+
+    } 
 }
